b60a016da165309309cf91469103da252d78b827
Syed Qasim Jalal <qgladiater@gmail.com>
2024-05-17 12:30:49 +0500

    implemented builder

---
+++ Restuarant Management System DP/Food Builder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restuarant_Management_System_DP
+{
+    class Food_Builder
+    {
+        public Food_Builder(bool hasCheese, bool hasPepperoni, bool hasBellPepper, bool hasBeef, bool hasChicken, bool hasSauces) {
+
+            HasCheese = hasCheese;
+
+        
+        
+        
+        }
+        public bool HasCheese { get; set; }
+        public bool hasPepperoni { get; set; }
+        public bool hasBellPepper { get; set; }
+        public bool hasBeef { get; set; }
+        public bool hasChicken { get; set; }
+        
+
+    }
+}
