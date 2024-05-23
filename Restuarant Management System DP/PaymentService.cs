afd19d0985872ca45d87c05577618d2ec01d64cd
Syed Qasim Jalal <qgladiater@gmail.com>
2024-05-23 20:50:24 +0500

    Run the project

--- Restuarant Management System DP/PaymentService.cs
+++ Restuarant Management System DP/PaymentService.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Restuarant_Management_System_DP
 {
-    class PaymentService
+    public class PaymentService
     {
+        private int cost;
+        private Boolean includeDelivery;
+
+
+        public void processOrder() 
+        {
+        
+        
+        }
+        private int getTotal()
+        {
+            return includeDelivery ? cost + 10 : cost;
+        
+        }
+
+
     }
 }
